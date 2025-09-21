4b98df635b3e743cd0be6cbb8b72b98e758e1755
Vincent Hilla <vincenthilla5@gmail.com>
2025-09-21 08:49:39 +0200

    remove tel from net

--- _data/metadata.js
+++ _data/metadata.js
@@ -9,7 +9,6 @@ export default {
 		name: "Vincent Hilla",
 		email: "vincenthilla5@gmail.com",
 		url: "https://vinhill.github.io",
-		tel: "+49 157 8969 8036",
 		linkedin: "https://linkedin.com/in/vincent-hilla",
 		github: "https://github.com/vinhill",
 		bugzilla: "https://bugzilla.mozilla.org/user_profile?user_id=725173",
